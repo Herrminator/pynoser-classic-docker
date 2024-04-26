e688ecc9606d01f4b61c13baab0dcc32c4f5fd78
Herrminator <hhofer69@gmail.com>
2024-04-26 22:00:35 +0200

    Optionally use default icons in icon update script

--- scripts/updicon_ng.py
+++ scripts/updicon_ng.py
@@ -6,9 +6,13 @@ from urlparse         import urlsplit, urlunsplit
 from runtime          import settings #@UnusedImport
 from reader.models    import Feed
 from reader           import feed
+from django.db.models import Q
 
 DEBUG   = True
 LOGFILE = sys.stderr
+
+DEFAULT_RSS_ICON = "https://cgi.johler.ph/static/icons/rss.png"
+DEFAULT_WEB_ICON = "https://cgi.johler.ph/static/icons/web.png"
 
 class PseudoFeed(object):
     etag = None
@@ -69,10 +73,12 @@ def main(argv=sys.argv):
     ap.add_argument(      "feeds",     nargs="*", type=int)
     ap.add_argument("-a", "--all",     default=False, action="store_true")
     ap.add_argument("-e", "--empty",   default=False, action="store_true", help="Only feeds with missing icons")
-    ap.add_argument("-c", "--clear",   default=False, action="store_true", help="Clear missing or erroneous icons")
     ap.add_argument("-d", "--debug",   default=False, action="store_true")
-    ap.add_argument("-D", "--dry",     default=False, action="store_true")
+    ap.add_argument(      "--dry",     default=False, action="store_true")
     ap.add_argument("-t", "--timeout", default=20.0,  type=float)
+    cd = ap.add_mutually_exclusive_group()
+    cd.add_argument("-c", "--clear",   default=False, action="store_true", help="Clear missing, erroneous or default icons")
+    cd.add_argument("-D", "--default", default=False, action="store_true", help="Set default icon for missing or erroneous icons")
     args = ap.parse_args()
 
     global DEBUG
@@ -84,36 +90,45 @@ def main(argv=sys.argv):
     if args.feeds:
         feeds = feeds.filter(id__in=args.feeds)
     if args.empty:
-        feeds = feeds.filter(feedstatus__icon__isnull=True)
+        feeds = feeds.filter(  Q(feedstatus__icon__isnull=True)
+                             | Q(feedstatus__icon=DEFAULT_RSS_ICON)
+                             | Q(feedstatus__icon=DEFAULT_WEB_ICON))
 
     for f in feeds:
         if f.isActive() or args.all:
             fstatus = f.status()
             icon = fstatus.icon
+            dflt = DEFAULT_RSS_ICON if f.feedType != feed.HTML else DEFAULT_WEB_ICON
+
             print("{0.id:4} {0.title:40.40} {1:72.72}".format(f, icon), end="")
             status = "ok"
             if f.feedType not in [ feed.FEED, feed.HTML ]:
                 status = "no" # Only for RSS and HTML feeds
             elif icon is None or icon == "":
                 status = "empty"
+            elif icon == dflt:
+                status = "dflt"
             else:
                 fetched, info, err = fetch(icon, debug=False)
                 if err:
                     status = "ERROR"
                 elif not fetched:
                     status = "empty"
-            print("{0}.".format(status))
+            print(" {0}.".format(status))
             if status not in  [ "ok", "no" ]:
                 neew = find_icon(f)
-                print("{0.id:4} {1:40.40} {2:72.72}".format(f, "-->", neew), end="")
                 if neew is not None:
                     status = "new"
                 elif args.clear:
                     status = "clear"
-                if (neew is not None or args.clear) and not args.dry:
-                    fstatus.icon = neew
-                    fstatus.save()
-                print("{0}.".format(status))
+                elif args.default:
+                    status = "dflt"
+                    neew = dflt
+                if neew != fstatus.icon:
+                    print("{0.id:4} {1:40.40} {2:72.72} {3}".format(f, "-->", neew, status))
+                    if (neew is not None or args.clear) and not args.dry:
+                        fstatus.icon = neew
+                        fstatus.save()
 
 
 if __name__ == "__main__":
